b47d1b5115c47932f42ccd56db85e1477d7ab18b
Diego Aguilar <diegoaguilar@Diegos-MacBook-Pro-2.local>
2017-03-26 07:56:34 -0700

    Assignment: 6 Services Collection

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,14 @@
          return albumPicasso;
      };
 
+     Fixtures.getCollection = function(numberOfAlbums) {
+       var array = [];
+       for (var i = 0; i < numberOfAlbums; i++) {
+         array.push(albumPicasso);
+       }
+       return array;
+      };
+
          return Fixtures;
      }
 
